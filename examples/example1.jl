967056fd876a5adafc91811a450d3d10ea07b0ac
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2020-02-21 22:32:15 +0100

    Reorganization of code structrue, attempt on Julian api

--- examples/example1.jl
+++ examples/example1.jl
@@ -58,6 +58,54 @@ function main(;nspin=50)
     write_vtk(griddata,"test1.vtk")
 end
 
+function main2(;nspin=25)
+    for i=1:2
+        
+        VTKFig.clear()
+        h=0.01
+        X=collect(0:h:1.0)
+        Y=collect(0:h:1.2)
+        Z=zeros(Float64,length(X),length(Y))
+        griddata=VTKFig.DataSet()
+        contour=VTKFig.ScalarView()
+        set_layout(2,1)
+        set_size(600,300)
+        set_rectilinear_grid(griddata,X,Y) 
+        
+        set_point_scalar(griddata,vec(Z),"V")
+        set_data(contour,griddata,"V")
+        add_figure(contour,1)
+        
+        plot=VTKFig.XYPlot()
+        add_figure(plot,2)
+    
+        
+        ii=1
+        t=0.0
+        dt=0.1
+        while ii<nspin
+            @time for i=1:length(X)
+                for j=1:length(Y)
+                    Z[i,j] = G(X[i],Y[j],t)
+                end
+            end
+            set_point_scalar(griddata,vec(Z),"V")
+            clear(plot)
+            set_plot_legend(plot,"j=1")
+            set_plot_color(plot,1,0,0)
+            add_plot(plot,X,Z[:,1])
+            
+            set_plot_color(plot,0,0,1)
+            set_plot_legend(plot,"i=1")
+            add_plot(plot,Y,Z[1,:])
+            
+            VTKFig.show()
+            t+=dt
+            ii+=1
+        end
+    end
+end
+
 
 
 
--- src/vtkfig_capi.jl
+++ src/vtkfig_capi.jl
@@ -125,7 +125,7 @@ destruct!(this::XYPlot)=ccall( ("vtkfigDestroyXYPlot",libvtkfig), Cvoid, (Ptr{Cv
 
 
 ###########################################################################
-
+# FRAME API
 interact(this::Frame)=ccall(("vtkfigInteract",libvtkfig),Cvoid,(Ptr{Cvoid},),this.cobj)
 
 show(this::Frame)=ccall(("vtkfigShow",libvtkfig),Cvoid,(Ptr{Cvoid},),this.cobj)
@@ -172,3 +172,71 @@ set_frame_title(this::Frame,title)=ccall(("vtkfigSetFrameTitle",libvtkfig), Cvoi
 
 set_position(this::Frame,x,y)=ccall(("vtkfigSetPosition",libvtkfig), Cvoid, (Ptr{Cvoid},Cint,Cint), this.cobj,x,y)
 
+#######################################################################################################################
+# DataSet API
+
+set_rectilinear_grid(this::DataSet, X::Vector{Cdouble},Y::Vector{Cdouble})=ccall( ("vtkfigSetRectilinearGrid2D",libvtkfig), Cvoid,(Ptr{Cvoid},Ptr{Cdouble},Cint,Ptr{Cdouble},Cint), this.cobj,X, length(X),Y,length(Y))
+
+set_rectilinear_grid(this::DataSet, X::Vector{Cdouble},Y::Vector{Cdouble},Z::Vector{Cdouble})=ccall( ("vtkfigSetRectilinearGrid3D",libvtkfig), Cvoid,(Ptr{Cvoid},Ptr{Cdouble},Cint,Ptr{Cdouble},Cint,Ptr{Cdouble},Cint), this.cobj,X,length(X),Y,length(Y),Z,length(Z))
+
+set_simplex_grid(this::DataSet, Points::Vector{Cdouble,2},Cells::Vector{Cint,2})=ccall( ("vtkfigSetSimplexGrid",libvtkfig), Cvoid,(Ptr{Cvoid},Cint,Ptr{Cdouble},Cint,Ptr{Cint},Cint), this.cobj,size(Points,1),Points,size(Points,2),Cells,size(Cells,2))
+
+set_point_scalar(this::DataSet, X::Vector{Cdouble},name)=ccall(("vtkfigSetPointScalar",libvtkfig), Cvoid, (Ptr{Cvoid},Ptr{Cdouble},Cint,Cstring), this.cobj,X, length(X),name)
+
+write_vtk(this::DataSet,fname; ftype="A")=ccall( ("vtkfigWriteVTK",libvtkfig), Cvoid, (Ptr{Cvoid},Cstring,Cstring,), this.cobj,fname,ftype)
+
+#######################################################################################################################
+# ScalarView API
+
+set_data(this::ScalarView, dataset::DataSet,name)=ccall(("vtkfigSetData",libvtkfig),Cvoid, (Ptr{Cvoid},Ptr{Cvoid},Cstring),this.cobj,dataset.cobj,name)
+
+set_isolevels(this::ScalarView, V::Vector{Cdouble})=ccall(("vtkfigSetIsolevels",libvtkfig), Cvoid, (Ptr{Cvoid},Ptr{Cdouble},Cint), this.cobj,V, length(V))
+
+show_isosurfaces(this::ScalarView,b::Bool)=ccall(("vtkfigShowIsosurfaces",libvtkfig), Cvoid,(Ptr{Cvoid},Cint), this.cobj,Int(b))
+
+#######################################################################################################################
+# XYPlot API
+
+add_plot(this::XYPlot, X::Vector{Cdouble},Y::Vector{Cdouble})=ccall(("vtkfigAddPlot",libvtkfig), Cvoid,(Ptr{Cvoid},Ptr{Cdouble},Cint,Ptr{Cdouble},Cint), this.cobj,X, length(X),Y,length(Y))
+    
+set_title(this::XYPlot,title)=ccall(("vtkfigSetTitle",libvtkfig),Cvoid,(Ptr{Cvoid},Cstring), this.cobj,title); 
+
+show_legend(this::XYPlot,b::Bool)=ccall(("vtkfigShowLegend",libvtkfig), Cvoid,(Ptr{Cvoid},Cint), this.cobj,Int(b))
+
+show_grid(this::XYPlot,b::Bool)=ccall(("vtkfigShowGrid",libvtkfig),Cvoid,(Ptr{Cvoid},Cint), this.cobj,Int(b))
+
+adjust_labels(this::XYPlot,b::Bool)=ccall(("vtkfigAdjustLabels",libvtkfig), Cvoid,(Ptr{Cvoid},Cint), this.cobj,Int(b))
+
+set_legend_position(this::XYPlot,x,y)=ccall(("vtkfigSetLegendPosition",libvtkfig), Cvoid,(Ptr{Cvoid},Cdouble, Cdouble,),this.cobj,x,y)
+
+set_legend_size(this::XYPlot,w,h)=ccall(("vtkfigSetLegendSize",libvtkfig),Cvoid,(Ptr{Cvoid},Cdouble, Cdouble,),this.cobj,w,h)
+
+clear(this::XYPlot)=ccall(("vtkfigClear",libvtkfig),Cvoid,(Ptr{Cvoid},),this.cobj)
+
+set_plot_linetype(this::XYPlot, ltype)=ccall(("vtkfigSetPlotLineType",libvtkfig),Cvoid,(Ptr{Cvoid},Cstring),this.cobj,ltype) 
+
+set_plot_markertype(this::XYPlot,mtype)=ccall(("vtkfigSetPlotMarkerType",libvtkfig), Cvoid,(Ptr{Cvoid},Cstring), this.cobj,mtype) 
+
+set_plot_color(this::XYPlot,r,g,b)=ccall(("vtkfigSetPlotColor",libvtkfig),Cvoid,(Ptr{Cvoid},Cdouble,Cdouble,Cdouble,),this.cobj,r,g,b) 
+
+set_plot_legend(this::XYPlot, legend)=ccall(("vtkfigSetPlotLegend",libvtkfig),Cvoid,(Ptr{Cvoid},Cstring,),this.cobj,legend) 
+
+set_grid_color(this::XYPlot,r,g,b)=ccall(("vtkfigSetGridColor",libvtkfig),Cvoid,(Ptr{Cvoid},Cdouble,Cdouble,Cdouble,), this.cobj,r,g,b) 
+
+set_axes_color(this::XYPlot,r,g,b)=ccall(("vtkfigSetAxesColor",libvtkfig),Cvoid,(Ptr{Cvoid},Cdouble,Cdouble,Cdouble,), this.cobj,r,g,b) 
+
+set_xtitle(this::XYPlot,title)=ccall(("vtkfigSetXTitle",libvtkfig),Cvoid,(Ptr{Cvoid},Cstring), this.cobj,title) 
+  
+set_ytitle(this::XYPlot,title)=ccall(("vtkfigSetYTitle",libvtkfig), Cvoid,(Ptr{Cvoid},Cstring), this.cobj,title) 
+
+set_linewidth(this::XYPlot,w)=ccall(("vtkfigSetLineWidth",libvtkfig), Cvoid,(Ptr{Cvoid},Cdouble,),this.cobj,w)
+
+set_markersize(this::XYPlot,s)=ccall(("vtkfigSetMarkerSize",libvtkfig),Cvoid,(Ptr{Cvoid},Cdouble,),this.cobj,s)
+
+set_xrange(this::XYPlot, x0,x1)=ccall(("vtkfigSetXRange",libvtkfig),Cvoid,(Ptr{Cvoid},Cdouble, Cdouble), this.cobj,x0,x1) 
+
+set_yrange(this::XYPlot, y0,y1)=ccall(("vtkfigSetYRange",libvtkfig),Cvoid,(Ptr{Cvoid},Cdouble, Cdouble), this.cobj,y0,y1) 
+
+set_number_of_xlabels(this::XYPlot,n)=ccall(("vtkfigSetNumberOfXLabels",libvtkfig), Cvoid,(Ptr{Cvoid},Cint,),this.cobj,n)
+
+set_number_of_ylabels(this::XYPlot,n)=ccall(("vtkfigSetNumberOfYLabels",libvtkfig),Cvoid,(Ptr{Cvoid},Cint,), this.cobj,n)
